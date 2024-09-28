9a6458ddf50d9f4377217b5225e88640970062af
Chaman Betrabet <betrabet.c@northeastern.edu>
2024-09-28 18:40:43 -0400

    Started

---
+++ helper.py
@@ -0,0 +1,7 @@
+class Helper:
+    def print(self, message):
+        print(message, end='')
+
+    def println(self, message):
+        print(message)
+
---
+++ solution.py
@@ -0,0 +1,11 @@
+from helper import Helper
+
+class Solution:
+
+    def init(self, helper, parking):
+        self.parking = parking
+        self.free_spots = {}
+
+    
+
+    
